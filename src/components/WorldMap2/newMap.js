1e389a27b31f26d6476d786ad784386e68412f5a
case-kim <kwkim9037@gmail.com>
2019-12-17 16:42:09 +0900

    Update Map Style
    1. /newmap 에 새 지도 첨부
    2. 링크...링크 어케 걸지 ㅠㅠ

--- src/components/WorldMap2/newMap.js
+++ src/components/WorldMap2/newMap.js
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from 'react';
 import ReactMapGL, {Marker, Popup, NavigationControl, FlyToInterpolator} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './styles.css'
-import {Paper} from '@material-ui/core'
+import {Container, Typography} from '@material-ui/core'
+import {throttle} from 'lodash';
 
 const cityList = [
   { name: 'New York', location: [40.7128, -74.0060] },
-  { name: '할리스', location: [37.564431, 126.986591] },
-  { name: '세븐일레븐', location: [37.565188, 126.983238] },
-  { name: '파리바게트', location: [37.564869, 126.984450] },
-  { name: '스타벅스', location: [37.562003, 126.985829] }
+  { name: 'Beijing', location: [39.9042, 116.4074] },
+  { name: 'Paris', location: [48.8566, 2.3522] },
+  { name: 'Cairo', location: [30.0444, 31.2357] },
+  { name: 'Seoul', location: [37.5665, 126.9780] }
 ];
-
-
 
 const Mapbox = () => {
   
@@ -20,13 +19,13 @@ const Mapbox = () => {
   const [ viewport, setViewport ] = useState({
     latitude: 37.532600,
     longitude: 127.024612,
-    width: '100vw',
-    height: '100vh',
+    width: '100%',
+    height: '100%',
     zoom: 2
   });
   
   useEffect(() => {
-    const mapResizeEvent = _.throttle(function() {
+    const mapResizeEvent = throttle(() => {
     setViewport(Object.assign({}, {
       ...viewport,
       width: `${window.innerWidth}px`,
@@ -41,8 +40,15 @@ const Mapbox = () => {
 
   const [selectedCity, setSelectedCity] = useState('');
   
-  return (
+  return (        
     <div className="Mapbox">
+      <div>
+          <Container maxWidth={false}>
+            <Typography component="h1" variant="h5" align="left" color="textPrimary">
+            Click your city
+            </Typography>
+          </Container>
+      </div>
         <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={MAP_TOKEN}
@@ -51,32 +57,44 @@ const Mapbox = () => {
         setViewport(viewport);
         }}
         >
+          
           <div className="navi-control">
             <NavigationControl/>
           </div>
           {
-            cityList.map((store, i) => (
+            cityList.map((city, i) => (
             <Marker
               key={i}
-              latitude={store.location[0]}
-              longitude={store.location[1]}
+              latitude={city.location[0]}
+              longitude={city.location[1]}
             >
             <button
               className="btn-marker"
+              onClick = {() => setSelectedCity(city)}
             />
             </Marker>
             ))
           }
           {
             selectedCity && (
-            <Popup
-              offsetLeft={10}
-              latitude={selectedCity.location[0]}
-              longitude={selectedCity.location[1]}
-              onClose={() => setSelectedCity('')}
-            >
-              <div>{selectedCity.name}</div>
-            </Popup>
+              <Popup
+                offsetLeft={10}
+                latitude={selectedCity.location[0]}
+                longitude={selectedCity.location[1]}
+                onClose={() => setSelectedCity('')}
+              >
+                <div>{selectedCity.name}{' '}|{' '}
+                  <a
+                    target ="_new"
+                    href = "../posts"
+                  >
+                    Posts
+                  </a>
+                </div>
+                <div>
+                  <img width={240} src='https://image.freepik.com/free-photo/beautiful-architecture-building-cityscape-seoul-city_74190-3218.jpg'></img>
+                </div>
+              </Popup>
             )
           }
         </ReactMapGL>
